11808cc3f0c4ce38c1c20b9b633f1451037cb5c0
denstiny <2254228017@qq.com>
2025-04-30 22:20:29 +0800

    feat: client start

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
 package main
 
+import (
+	"github.com/denstiny/golang-language-server/pkg/engine"
+	"github.com/denstiny/golang-language-server/pkg/route"
+)
+
 func main() {
+	r := route.NewRoute()
+	client := engine.NewClient(r)
+	client.Start()
+}
